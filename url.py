28ae8a18aa38cc5d19b927f5a32685fad284e856
felix <lokling@gmail.com>
2015-04-18 23:17:58 +0800

    added proxy for scraping backpacker.com.tw

--- url.py
+++ url.py
@@ -1,13 +1,17 @@
 import urllib2
+import socket
+
 
 def get(url):
-	#proxy = urllib2.ProxyHandler({'http':'116.77.128.68:8118'})
-	#opener = urllib2.build_opener(proxy)
-	#urllib2.install_opener(opener)
+    if(socket.gethostname() == "homestead"):
+        proxy = urllib2.ProxyHandler({'http':'111.1.36.163:80'})
+        opener = urllib2.build_opener(proxy)
+        urllib2.install_opener(opener)
 
-	hdr = {'Accept-Encoding': 'deflate, sdch',
-	       'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh-TW;q=0.4',
-	       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36'}
-	req = urllib2.Request(url, headers=hdr)
-	response = urllib2.urlopen(req)
-	return response
+    hdr = {'Accept-Encoding': 'deflate, sdch',
+            'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh-TW;q=0.4',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36'}
+    req = urllib2.Request(url, headers=hdr)
+    response = urllib2.urlopen(req)
+    print 'ok'
+    return response
