4546f19fa3f98ca39944b01171a95995acd6e048
felix <lokling@gmail.com>
2015-04-20 01:21:17 +0800

    improve catching exception , log traceback, change tab to 4 spaces

--- scraper/backpackers_com_tw.py
+++ scraper/backpackers_com_tw.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 import logger
+import traceback
 
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -10,63 +11,68 @@ import dbagent
 import url
 
 class Scraper:
-	def __init__(self):
-		self._scraper_id = 1
-		dbagent.set_time_zone('+00:00')
+    def __init__(self):
+        self._scraper_id = 1
+        dbagent.set_time_zone('+00:00')
 
-	def _scrape(self, src_id, src_url):
-		xml_doc = url.get(src_url).read().decode('utf-8','ignore')
-		soup = BeautifulSoup(xml_doc, "xml")
+    def _scrape(self, src_id, src_url):
+        try:
+            xml_doc = url.get(src_url).read().decode('utf-8','ignore')
+            soup = BeautifulSoup(xml_doc, "xml")
 
-		latest = dbagent.get_latest_time(src_id)
-		post_id = self._get_post_id(src_url)
+            latest = dbagent.get_latest_time(src_id)
+            post_id = self._get_post_id(src_url)
 
-		scape_count = 0
+            scape_count = 0
 
-		for item in soup('item'):
-			pub_date = item.pubDate.string
-			postedAt = datetime.strptime(pub_date,'%a, %d %b %Y %H:%M:%S GMT')
+            for item in soup('item'):
+                pub_date = item.pubDate.string
+                postedAt = datetime.strptime(pub_date,'%a, %d %b %Y %H:%M:%S GMT')
 
-			link_post_id = self._get_post_id(item.category['domain'])
+                link_post_id = self._get_post_id(item.category['domain'])
 
-			if latest == None or latest < postedAt:
-				if post_id == link_post_id:
-					title = item.title.string
-					link = item.link.string
-					content = item.description.string
-					author = ''
-					remote_id = 0
-					try:
-						print ('scraper(%d), src(%d): id: %s' % (self._scraper_id, src_id, self._get_post_id(link)))
-						new_post_id = dbagent.add_post(title, content, link, author, src_id, remote_id, postedAt)
-						self._add_tags(src_id, new_post_id)
-						scape_count += 1
-					except Exception as e:
-						logger.log(	'error', 'scraper(%d), src(%d), id(%s): %s' 
-							% (self._scraper_id, src_id, self._get_post_id(link), e))
-			else:
-				break
+                if latest == None or latest < postedAt:
+                    if post_id == link_post_id:
+                        try:
+                            title = item.title.string
+                            link = item.link.string
+                            content = item.description.string
+                            author = ''
+                            remote_id = 0
+                            print ('scraper(%d), src(%d): id: %s' % (self._scraper_id, src_id, self._get_post_id(link)))
+                            new_post_id = dbagent.add_post(title, content, link, author, src_id, remote_id, postedAt)
+                            self._add_tags(src_id, new_post_id)
+                            scape_count += 1
+                        except Exception, err:
+                            logger.log(    'error', 'scraper(%d), src(%d), id(%s):\n%s' 
+                                % (self._scraper_id, src_id, self._get_post_id(link), traceback.format_exc()))
+                else:
+                    break
 
-		logger.log(	'info', 'scraper(%d), src(%d): %d posts saved' 
-					% (self._scraper_id, src_id, scape_count))
+            logger.log(    'info', 'scraper(%d), src(%d): %d posts saved' 
+                        % (self._scraper_id, src_id, scape_count))
 
-	def _get_post_id(self, url):
-		prog = re.compile('^.+=(\d+)$')
-		result = prog.match(url)
-		return result.group(1)
+        except Exception, err:
+            logger.log(    'error', 'scraper(%d), src(%d):\n%s' 
+                % (self._scraper_id, src_id, traceback.format_exc()))
 
-	def _add_tags(self, source_id, post_id):
-		for tag_id in dbagent.get_source_tags(source_id):
-			dbagent.add_post_tag(post_id, tag_id)
+    def _get_post_id(self, url):
+        prog = re.compile('^.+=(\d+)$')
+        result = prog.match(url)
+        return result.group(1)
+
+    def _add_tags(self, source_id, post_id):
+        for tag_id in dbagent.get_source_tags(source_id):
+            dbagent.add_post_tag(post_id, tag_id)
 
 
-	def scrape(self):
-		for src in dbagent.get_sources(self._scraper_id):
-			self._scrape(src['id'], src['url'])
+    def scrape(self):
+        for src in dbagent.get_sources(self._scraper_id):
+            self._scrape(src['id'], src['url'])
 
-		#self._scrape(27, 'http://www.backpackers.com.tw/forum/external.php?type=RSS2&amp;forumids=309')
-		
-		#print(self._sources())
-		#for src in self._sources():
-		#	self._scrape(src)
-		
+        #self._scrape(27, 'http://www.backpackers.com.tw/forum/external.php?type=RSS2&amp;forumids=309')
+        
+        #print(self._sources())
+        #for src in self._sources():
+        #    self._scrape(src)
+        
--- scraper/facebook_com.py
+++ scraper/facebook_com.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
+import traceback
 import simplejson
 
 from datetime import datetime
@@ -10,65 +11,69 @@ import url
 import logger
 
 class Scraper:
-	def __init__(self):
-		self._scraper_id = 2
-		self._token = "CAALN1209ZAmgBAMpVBq7m8yDo7hj5ThZCZC67IVUUoq1ZBOoOOe4MNETUD1w38uNDzUzZCu2nTfKu87rLwPwRVgwNrtl7hZC6O5VFvv622PWDxeiEOl73p5TujuyvWzKo3bYRCnTZAnIW0PWZBGmi2Oc79S2xvinKJBfcNFZBKzMuBn5hvTROMUDl"
-		dbagent.set_time_zone('+00:00')
+    def __init__(self):
+        self._scraper_id = 2
+        self._token = "CAALN1209ZAmgBAMpVBq7m8yDo7hj5ThZCZC67IVUUoq1ZBOoOOe4MNETUD1w38uNDzUzZCu2nTfKu87rLwPwRVgwNrtl7hZC6O5VFvv622PWDxeiEOl73p5TujuyvWzKo3bYRCnTZAnIW0PWZBGmi2Oc79S2xvinKJBfcNFZBKzMuBn5hvTROMUDl"
+        dbagent.set_time_zone('+00:00')
 
-	def _scrape(self, src_id, src_remote_id):
-		src_url = ("https://graph.facebook.com/v2.3/%s/feed?access_token=%s" 
-				% (src_remote_id, self._token))
+    def _scrape(self, src_id, src_remote_id):
+        try:
+            src_url = ("https://graph.facebook.com/v2.3/%s/feed?access_token=%s" 
+                    % (src_remote_id, self._token))
 
-		scape_count = 0
-		json = simplejson.load(url.get(src_url))
+            scape_count = 0
+            json = simplejson.load(url.get(src_url))
 
-		for post in json['data']:
-		    remote_id = post['id']
-		    if(not dbagent.is_post_exist(src_id, remote_id)):
-			    if('message' in post):
-			        splits = post['message'].split('\n',1)
-			        title = splits[0]
-			        content = splits[1] if (len(splits) > 1) else ''
-			    elif('description' in post):
-			        splits = post['description'].split('\n',1)
-			        title = splits[0]
-			        content = splits[1] if (len(splits) > 1) else ''
-			    else:
-			        title = '(請點連結閱讀內容)'
-			        content = ''
-			    title = title[:30] if (len(title) > 30) else title
-			    
-			    link = "https://www.facebook.com/"+post['id']
-			    author = post['from']['name']
-			    posted_at = datetime.strptime(post['created_time'],'%Y-%m-%dT%H:%M:%S+0000')
-			    try:
-					print ('scraper(%d), src(%d): id: %s' % (self._scraper_id, src_id, remote_id))
-					new_post_id = dbagent.add_post(title, content, link, author, src_id, remote_id, posted_at)
-					self._add_tags(src_id, new_post_id)
-					scape_count += 1
-			    except Exception as e:
-						logger.log(	'error', 'scraper(%d), src(%d), id(%s): %s' 
-							% (self._scraper_id, src_id, post['id'], e))
+            for post in json['data']:
+                remote_id = post['id']
+                if(not dbagent.is_post_exist(src_id, remote_id)):
+                    try:
+                        if('message' in post):
+                            splits = post['message'].split('\n',1)
+                            title = splits[0]
+                            content = splits[1] if (len(splits) > 1) else ''
+                        elif('description' in post):
+                            splits = post['description'].split('\n',1)
+                            title = splits[0]
+                            content = splits[1] if (len(splits) > 1) else ''
+                        else:
+                            title = '(請點連結閱讀內容)'
+                            content = ''
+                        title = title[:30] if (len(title) > 30) else title
+                        
+                        link = "https://www.facebook.com/"+post['id']
+                        author = post['from']['name']
+                        posted_at = datetime.strptime(post['created_time'],'%Y-%m-%dT%H:%M:%S+0000')
+                        print ('scraper(%d), src(%d): id: %s' % (self._scraper_id, src_id, remote_id))
+                        new_post_id = dbagent.add_post(title, content, link, author, src_id, remote_id, posted_at)
+                        self._add_tags(src_id, new_post_id)
+                        scape_count += 1
+                    except Exception, err:
+                        logger.log(    'error', 'scraper(%d), src(%d), id(%s):\n%s' 
+                                    % (self._scraper_id, src_id, self._get_post_id(link), traceback.format_exc()))
 
-		logger.log(	'info', 'scraper(%d), src(%d): %d posts saved' 
-					% (self._scraper_id, src_id, scape_count))
+            logger.log(    'info', 'scraper(%d), src(%d): %d posts saved' 
+                        % (self._scraper_id, src_id, scape_count))
+        except Exception, err:
+            logger.log(    'error', 'scraper(%d), src(%d):\n%s' 
+                % (self._scraper_id, src_id, traceback.format_exc()))
 
-	# def _get_post_id(self, url):
-	# 	prog = re.compile('^.+=(\d+)$')
-	# 	result = prog.match(url)
-	# 	return result.group(1)
+    # def _get_post_id(self, url):
+    #     prog = re.compile('^.+=(\d+)$')
+    #     result = prog.match(url)
+    #     return result.group(1)
 
-	def _add_tags(self, source_id, post_id):
-		for tag_id in dbagent.get_source_tags(source_id):
-			dbagent.add_post_tag(post_id, tag_id)
+    def _add_tags(self, source_id, post_id):
+        for tag_id in dbagent.get_source_tags(source_id):
+            dbagent.add_post_tag(post_id, tag_id)
 
-	def scrape(self):
-		for src in dbagent.get_sources(self._scraper_id):
-			self._scrape(src['id'], src['url'])
+    def scrape(self):
+        for src in dbagent.get_sources(self._scraper_id):
+            self._scrape(src['id'], src['url'])
 
-		#self._scrape(27, 'http://www.backpackers.com.tw/forum/external.php?type=RSS2&amp;forumids=309')
-		
-		#print(self._sources())
-		#for src in self._sources():
-		#	self._scrape(src)
-		
+        #self._scrape(27, 'http://www.backpackers.com.tw/forum/external.php?type=RSS2&amp;forumids=309')
+        
+        #print(self._sources())
+        #for src in self._sources():
+        #    self._scrape(src)
+        
