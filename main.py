dc2895a157a19eb2a8738511df411a94e942c2a6
felix <lokling@gmail.com>
2015-05-01 17:27:45 +0800

    added scraper start and stop log

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import time
 import datetime
 import logging
 while 1:
+
+    #log file setup
     now = datetime.datetime.utcnow()
     log_file_name = now.strftime('%Y%m%d')
 
@@ -16,10 +18,16 @@ while 1:
     log.addHandler(fileh)      # set the new handler
     log.setLevel(logging.INFO)
 
+    #scrape
+
+    logging.info('Scraper(%d) started scraping', 1)
     s = backpackers_com_tw.Scraper()
     s.scrape()
+    logging.info('Scraper(%d) started scraping', 1)
 
+    logging.info('Scraper(%d) stopped scraping', 2)
     s = facebook_com.Scraper()
     s.scrape()
+    logging.info('Scraper(%d) stopped scraping', 2)
 
     time.sleep(3*60)
